15a062d3c913277bddc1c8e791cffd3164ff6431
ティオ神保町 <tiojinbouchou@tiojinbouchou-no-iMac-2.local>
2019-02-26 15:24:25 +0900

    ちょっと疲れ気味だけどDB始めたよ

---
+++ chap4/sample4-6_codeEscape.sql
@@ -0,0 +1,8 @@
+-- CREATEを学ぶ
+-- ・テーブルを作成する
+
+CREATE TABLE my_item(id INT, item_name TEXT, price INT);
+-- CREATE TABLE 作成するテーブルの名前(カラム名 カラムの型(INT,TEXT など), カラム名 カラムの型(INT,TEXT など),…以下同文繰り返し);
+
+-- データベースへ行って、「構造(structure)」へ飛べば、
+-- 上の関数で作ったカラムがデータベース内にある。

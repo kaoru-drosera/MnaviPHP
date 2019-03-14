97de27c9bcb31f7c89235a68d85b086e6b1a9a1b
ティオ神保町 <tiojinbouchou@tiojinbouchou-no-iMac-2.local>
2019-03-14 15:04:35 +0900

    ???

--- chap3/sample3-27/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (rand(0,1) == 0){
-  header('Location: a.html');
-} else {
-  header('Location: b.html');
-}
-// ランダムな値 = rand(最小の数字,最大の数字);
-?>
--- chap4/sample4-6_codeEscape.sql
+++
@@ -1,8 +0,0 @@
--- CREATEを学ぶ
--- ・テーブルを作成する
-
-CREATE TABLE my_item(id INT, item_name TEXT, price INT);
--- CREATE TABLE 作成するテーブルの名前(カラム名 カラムの型(INT,TEXT など), カラム名 カラムの型(INT,TEXT など),…以下同文繰り返し);
-
--- データベースへ行って、「構造(structure)」へ飛べば、
--- 上の関数で作ったカラムがデータベース内にある。

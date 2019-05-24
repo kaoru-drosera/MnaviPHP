4b5e36cd946a4531f5bc1e24e72046dd138d2f20
ティオ神保町 <tiojinbouchou@tiojinbouchou-no-iMac-2.local>
2019-05-24 11:14:35 +0900

    一部分CSSが効くようにした

--- MnavPHP/chap5/memo/5-7/5-7index.php
+++ MnavPHP/chap5/memo/5-7/5-7index.php
@@ -8,8 +8,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/MnavPHP/MnaviPHP/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/MnavPHP/MnaviPHP/css/style.css">
+  <link rel="stylesheet" href="../../../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../../css/style.css">
   <title>よくわかるPHP 第5章</title>
 </head>
 <body>

2146ab7d399b24ef6a4efcaccb7a035492b22a7a
ティオ神保町 <tiojinbouchou@tiojinbouchou-no-iMac-2.local>
2019-05-27 15:00:02 +0900

    …zzzz…………

--- asciiJQ/chap2/asciiChap2-3/asciiChap2-3.js
+++ asciiJQ/chap2/asciiChap2-3/asciiChap2-3.js
@@ -48,4 +48,92 @@ $(document).ready(function(){
     return false;
   });
 
+
+
+  // mouseupとmousedownがよくわからない…。
+
+  $(".ul2-3 a").mousedown(function(){
+    $(".C2-3Img").attr("src",$(this).attr("href")).attr("alt",$(this).text());
+  }).click(function(){
+    return false;
+  });
+
+  $(".ul2-4 a").mouseup(function(){
+    $(".C2-4Img").attr("src",$(this).attr("href")).attr("alt",$(this).text());
+  }).click(function(){
+    return false;
+  });
+
+  // mouseupとmousedownがよくわからない…。
+
+
+
+  $(".C2-5Img").mouseover(function(){
+    $(this).attr("src","img/sea.jpg").attr("alt","海");
+  }).mouseout(function(){
+    $(this).attr("src","img/flower.jpg").attr("alt","どっか花");
+  });
+
+  $(".C2-6Img").mousemove(function(e){
+    $(".2-6mator1").html("現在のX座標:"+e.clientX);
+    $(".2-6mator2").html("現在のY座標:"+e.clientY);
+    // 「e.clientX」で「X座標」を、
+    // 「e.clientY」で「Y座標」を
+    // 検出できる。
+  });
+
+  $(".ul2-7 a").one("click",function(){
+    // 「one」は「一度だけ実行される処理」
+    $(".C2-7Img").attr("src",$(this).attr("href")).attr("alt",$(this).text());
+    return false;
+  });
+
+
+  $(".ul2-8").on("click","a",function(){
+    $(".C2-8Img").attr("src",$(this).attr("href")).attr("alt",$(this).text());
+    return false;
+    // $(セレクター).on("イベント発生の条件","イベントが発生する要素",function(){
+    //   // イベント発生時に実行する処理
+    // });
+  });
+  $(".ul2-9").on("click","a",function(){
+    $(".C2-9Img").attr("src",$(this).attr("href")).attr("alt",$(this).text());
+    return false;
+    // $(セレクター).on("イベント発生の条件","イベントが発生する要素",function(){
+    //   // イベント発生時に実行する処理
+    // });
+  });
+
+  $(".2-9addImg").on('click',function(){
+    $(".ul2-9").prepend('<li><a href="img/flower.jpg">どっか花</li>');
+  });
+
+
+  $(".ul2-10 a").click(function(){
+    $(".C2-10Img").attr("src",$(this).attr("href")).attr("alt",$(this).text());
+    return false;
+    // $(セレクター).on("イベント発生の条件","イベントが発生する要素",function(){
+    //   // イベント発生時に実行する処理
+    // });
+  });
+  $(".2-10off").click(function(){
+    $(".cont2-10 a").off("click");
+  });
+  // 「on('click'…)」は効かないんだろうか
+  // 一応、こんな式に。
+  // $(セレクタ).off("イベント発生の条件");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
---
+++ asciiJQ/chap2/asciiChap2-4/asciiChap2-4.js
@@ -0,0 +1,116 @@
+$(function(){
+  $(".cont4-1 button").click(function(){
+    $(".cont4-1").text("【"+$(".cont4-1 input").val()+"】にメールを送信します")
+    // 「val()」で、フォーム上の値を受け取れる。
+    // 「select」要素、「textarea」要素の値もこれで受け取れるぞ。
+    // $("select").val();
+    // $("textarea").val();
+  });
+
+
+  $(".cont4-2 button").click(function(){
+    $(".cont4-2 input").val("");
+  });
+
+
+  $(".cont4-3 input").val("入力してください").css({
+    "color":"red",
+    "background-color":"pink",
+  }).focus(function(){
+    $(this).val("").css({
+      "color":"black",
+      "background-color":"white",
+    });
+  });
+
+
+  $(".cont4-4 input").val("入力してください").css({
+    "color":"red",
+    "background-color":"pink",
+  }).one('focus',function(){
+    $(this).val("").css({
+      "color":"black",
+      "background-color":"white",
+    });
+  });
+
+
+  $(".cont4-5 input").val("入力してください").css({
+    "color":"red",
+  }).one('focus',function(){
+    $(this).val("").css({"color":"black"}).blur(function(){
+      if($(this).val() == ""){
+        $(this).val("入力してください").css({"color":"red"}).one('focus',function(){
+          $(this).val("").css({"color":"darkgray"});
+        });
+      }
+    });
+  });
+
+
+  $(".cont4-5_2 input").val("入力してください").css({
+    "color":"red",
+    "background-color":"white",
+  }).one('focus',function(){
+    $(this).val("").css({
+      "color":"black",
+      "background-color":"white",
+    }).blur(function(){
+      if($(this).val() == ""){
+        $(this).val("入力してください").css({
+          "color":"red",
+          "background-color":"pink",
+        }).one('focus',function(){
+          $(this).val("").css({
+            "color":"black",
+            "background-color":"white",
+          });
+        });
+      }
+    });
+  });
+
+ // oneのfunctionからうまくいかなくてよくわからなくて
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

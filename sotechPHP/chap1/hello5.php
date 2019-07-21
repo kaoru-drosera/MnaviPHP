250ed31c48b2df85306824837c27955ff3943eb1
kaoru-drosera <fdsdteolx@gmail.com>
2019-07-22 01:20:45 +0900

    花になって蝶になって夜になって踊れ

--- sotechPHP/chap1/hello5.php
+++ sotechPHP/chap1/hello5.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>定数</title>
+  <title>計算ちょっと</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
   <meta name="author" content="">
@@ -14,41 +14,26 @@
 </head>
 <body>
   <div class="main-contents">
-    <p>複数の値を出力するには、「echo "文字列","文字列",変数…」</p>
     <?php
-    $thord = 40;
-    echo "威力",$thord,"の","ソード";
-    echo "<br>";
-    ?>
-    <p>または、「print(変数."文字列…")」。</p>
-    <?php
-    $shotGun = 20;
-    $wep = "ショットガン";
-    print("威力".$shotGun."の".$wep);
-    echo "<br>";
-     ?>
-  </div><!--  .main-contents -->
-  <div class="main-contents">
-    <p>「print_r(配列名)」で、配列の値を順番に抜き出してくれるぞ。</p>
-    <?php
-    $colors = array("red","blue","yell","green");
-    $now = new DateTime();
-    print_r($colors);
-    print("<br>");
-    print_r($now);
+      $who = "青縞";
+      $hello = "hshs";
+      $msg = $who."たん".$hello.PHP_EOL;
+      echo $msg;
+      echo "<br>";
 
+      $num = 19 + 1;
+      $msg1 = $num."番目".PHP_EOL;
+      // ↑PHP_EOLは、「osに応じた改行を自動的に割り当てる定数」。
+      // …だそうだが、効果がない。
+      // $msg2 = $num.77;
+      $msg2 = $num."77";
+      echo $msg1;
+      echo "<br>";
+      echo $msg2;
+      // msg2は「.77」といった感じで連結できないのでスキップ。
+      // syntax error, unexpected '.77'
+      // 77を""で囲うことでなんとかテキストのような結果にできた。
      ?>
-     <pre>
-       <?php
-       $colors = array("red","blue","yell","green");
-       $now = new DateTime();
-       print_r($colors);
-       print("<br>");
-       print_r($now);
-
-        ?>
-     </pre>
-     <p>『＜pre＞＜/pre＞』で囲うことで、改行付きで表示できるぞ！</p>
   </div><!--  .main-contents -->
 </body>
 </html>
